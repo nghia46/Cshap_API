0ab473e9591e148c2bc56967141dacb7a2267bd5
NghiaLov_G <84240893+tiptopdragon0406@users.noreply.github.com>
2023-06-11 16:30:17 +0700

    Add files via upload

---
+++ ServiceBase.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository.Service
+{
+    public class ServiceBase<T> where T : class
+    {
+        private readonly API_TEST_DBContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public ServiceBase()
+        {
+            _context = new API_TEST_DBContext();
+            _dbSet = _context.Set<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public List<T> Search(Func<T, bool> predicate)
+        {
+            return _dbSet.Where(predicate).ToList();
+        }
+    }
+}
